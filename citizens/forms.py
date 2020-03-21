3f8d9eef584dfa9acea5fa5a164822982e20b146
Michael Perk <y0079915@tu-braunschweig.de>
2020-03-21 15:47:05 +0100

    Redesigned Create Form

--- citizens/forms.py
+++ citizens/forms.py
@@ -1,27 +1,31 @@
 from django import forms
 from .models import Citizen
+from phonenumber_field.formfields import PhoneNumberField
 
 
 class CitizenForm(forms.ModelForm):
     first_name = forms.CharField(widget=forms.TextInput(),
-                                 label="Vorname",
-                                 help_text="Bitte geben sie ihren vollständigen Vornamen ein")
+                                 label="Vorname")
     last_name = forms.CharField(widget=forms.TextInput(),
                                 label="Nachname",)
 
     email = forms.CharField(widget=forms.TextInput(),
-                            label="Email",)
+                            label="Email", help_text="Über diese Adresse erhalten Sie alle Informationen zum Testergebnis")
+
+    telephone = PhoneNumberField(label="Telefonnummer", help_text="Für eventuelle Rückfragen vom Gesundheitsamt")
 
     date_of_birth = forms.DateField(
         widget=forms.TextInput(
             attrs={'type': 'date'}
         ),
         label="Geburtsdatum",
+
     )
 
     address = forms.CharField(
         label='Adresse',
-        widget=forms.TextInput(attrs={'placeholder': 'Bohlweg 19'}),
+        widget=forms.TextInput(attrs={}),
+        help_text="Strassenname und Hausnummer"
     )
 
     city = forms.CharField(label="Stadt")
@@ -29,4 +33,4 @@ class CitizenForm(forms.ModelForm):
 
     class Meta:
         model = Citizen
-        fields = ('first_name', 'last_name', 'email', 'date_of_birth', 'address', 'city', 'zip_code')
+        fields = ('first_name', 'last_name', 'email', 'telephone', 'date_of_birth', 'address', 'city', 'zip_code')
