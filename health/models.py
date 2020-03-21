e76e753fb70f255158a024e25e8351f7b1008181
Michael Perk <y0079915@tu-braunschweig.de>
2020-03-21 12:26:39 +0100

    New Test Model

--- health/models.py
+++ health/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from citizens.models import Citizen
 
 
 class User(AbstractUser):
+
+    display_name = models.CharField(max_length=30, default="Anonym")
 
     is_health_department = models.BooleanField(default=False)
     is_laboratory = models.BooleanField(default=False)
@@ -10,3 +13,40 @@ class User(AbstractUser):
 
     longitude = models.FloatField()
     latitude = models.FloatField()
+
+
+class Test(models.Model):
+    POSITIVE = 1
+    NEGATIVE = 0
+    PENDING = -1
+
+    RESULT_CHOICES = [
+        (POSITIVE, 'Positive'),
+        (NEGATIVE, 'Negative'),
+        (PENDING, 'Pending'),
+    ]
+
+    citizen = models.OneToOneField(Citizen, on_delete=models.CASCADE, related_name="test")
+
+    test_station = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commissioned_tests")
+    laboratory = models.ForeignKey(User, on_delete=models.CASCADE, related_name="examined_tests")
+
+    result = models.IntegerField(
+        choices=RESULT_CHOICES,
+        default=PENDING,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @property
+    def is_positive(self):
+        return self.result == Test.POSITIVE
+
+    @property
+    def is_negative(self):
+        return self.result == Test.NEGATIVE
+
+    @property
+    def is_pending(self):
+        return self.result == Test.PENDING
